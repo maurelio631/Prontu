89c0fb6be2da66008b589813eef5924aa5f6210c
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2024-05-23 12:02:07 -0300

    sidebar reponsividade

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import Logo from "../assets/logo.svg";
 
 export default function Sidebar() {
+  const [isVisible, setIsVisible] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsVisible(window.innerWidth >= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="flex flex-col w-28 min-h-screen bg-[#efefef]">
-      <img className="w-28 py-10 px-4" src={Logo} alt="Logo Prontu e Ponto" />
-    </div>
+    isVisible && (
+      <div className="flex flex-col w-28 min-h-screen bg-[#efefef]">
+        <img className="w-28 py-10 px-4" src={Logo} alt="Logo Prontu e Ponto" />
+      </div>
+    )
   );
 }
