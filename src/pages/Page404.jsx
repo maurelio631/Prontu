c5657af137d8dc1b44cd57fc9f7fe53106b974a5
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2024-05-29 14:12:43 -0300

    page404 semipronta

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
+import  erroGif  from "../assets/erro404.gif";
+import  erroSvg  from "../assets/erro404.svg";
 
 export function Page404() {
     return(
-        <>
-            <Link to={'/Aquiro'}>Form</Link><br/>
-            <Link to={'/login'}>Login</Link><br />
-            <Link to={'/cadastro'}>Cadastro</Link><br />
-            <Link to={'/home/pacientes'}>Pacientes</Link><br />
-            <Link to={'/home/prontuario'}>Prontuario</Link><br/>
-            <Link to={'/home/ajustes'}>ajustes</Link><br/>
+        <div className="flex flex-col items-center p-20">
 
-            <Link to={'/home/agenda'}>Agenda</Link>
-        </>
+            <img src={erroSvg} alt=""  className="w-full max-w-60"/>
+            <h1 className="text-6xl font-bold text-azul-principal mb-5">Opa!</h1>
+            <p className="text-3xl font-normal text-[#404040] text-center">
+                Não encontramos a página que você tentou acessar
+            </p>
+
+            <div className="flex gap-5 pt-4 w-full justify-center flex-wrap">
+                <Link className="bg-azul-principal p-2 text-white rounded-xl font-semibold" to={'/Aquiro'}>Form</Link>
+                <Link className="bg-azul-principal p-2 text-white rounded-xl font-semibold" to={'/login'}>Login</Link>
+                <Link className="bg-azul-principal p-2 text-white rounded-xl font-semibold" to={'/cadastro'}>Cadastro</Link>
+                <Link className="bg-azul-principal p-2 text-white rounded-xl font-semibold" to={'/home/pacientes'}>Pacientes</Link>
+                <Link className="bg-azul-principal p-2 text-white rounded-xl font-semibold" to={'/home/prontuario'}>Prontuario</Link>
+                <Link className="bg-azul-principal p-2 text-white rounded-xl font-semibold" to={'/home/ajustes'}>Ajustes</Link>
+                <Link className="bg-azul-principal p-2 text-white rounded-xl font-semibold" to={'/home/agenda'}>Agenda</Link>
+            </div>
+
+
+        </div>
 
     )
 }
