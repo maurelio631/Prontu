191e90ba9d7cd30e4a79c88f34d97fb6c8bb4e84
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2024-06-29 12:34:06 -0300

    melhorando o carregamento pos ações

--- src/components/optionsPanel/Access.jsx
+++ src/components/optionsPanel/Access.jsx
@@ -10,16 +10,22 @@ import { LoadingIcon } from "../LoadingIcon";
 
 export function Access() {
     const [access, setAccess] = useState([]);
-    const [newAccess, setNewAccess] = useState({});
+    const [newAccess, setNewAccess] = useState({ name: '', email: '', role: '' });
     const [loading, setLoading] = useState(true);
     const [loadingForm, setLoadingForm] = useState(false);
     const { refreshToken } = useUser();
+    const [refreshKey, setRefreshKey] = useState(0);
 
     const changeNewAccess = (id, value) => {
         setNewAccess(prevData => ({
             ...prevData,
             [id]: value,
         }));
+    };
+
+    const changeNewAccessList = (event) => {
+        let val = event.target.value;
+        changeNewAccess('role', val.toLowerCase());
     };
 
     const getAccess = async (retryCount = 0) => {
@@ -42,7 +48,8 @@ export function Access() {
             setLoadingForm(true);
             await axios.post('/registerUsers', newAccess);
             toastSuccessAlert('Usuário criado com sucesso!');
-            window.location.reload();
+            clearForm();
+            setRefreshKey(prevKey => prevKey + 1);
             setLoadingForm(false);
         } catch (err) {
             if (err.response && err.response.status === 401 && retryCount < 1) {
@@ -56,36 +63,45 @@ export function Access() {
         }
     };
 
+    const confirmDel = (id) => {
+        confirmAlert('Deseja realmente deletar esse usuário?', 'Sim', 'Cancelar', () => deleteAccess(id));
+    }
+
     const deleteAccess = async (id, retryCount = 0) => {
         try {
             await axios.delete(`/deleteUser/${id}`);
             toastSuccessAlert('Usuário deletado com sucesso!');
-            window.location.reload();
+            setRefreshKey(prevKey => prevKey + 1);
         } catch (err) {
             if (err.response && err.response.status === 401 && retryCount < 1) {
                 await refreshToken();
                 deleteAccess(id, retryCount + 1);
-            } 
+            }
         }
     }
 
     const validateForm = () => {
+        console.log(newAccess);
         if (!newAccess.name || !newAccess.email || !newAccess.role) {
             return toastErrorAlert('Preencha todos os campos');
+        } else if (newAccess.role !== 'secretaria' && newAccess.role !== 'quiropraxista') {
+            return toastErrorAlert('O tipo de acesso deve ser "Secretaria" ou "Quiropraxista"');
         } else {
             addAccess();
         }
     }
 
+    const clearForm = () => {
+        setNewAccess({
+            name: '',
+            email: '',
+            role: ''
+        });
+    }
+
     useEffect(() => {
         getAccess();
-    }, []);
-
-    const handleSelectChange = (event) => {
-        const { id, value } = event.target;
-        changeNewAccess(id, value);
-    };
-    
+    }, [refreshKey]);
 
     return (
         <section className="overflow-y-auto max-h-full px-1 pb-20">
@@ -93,34 +109,34 @@ export function Access() {
             <p className="mb-5">Você poderá escolher até 5 acessos para o aplicativo.</p>
 
             <div className="flex flex-col gap-5 max-w-4xl max-h-64 p-2 overflow-y-auto">
-                {loading 
-                    ? <Loading minSize={'h-32'} /> 
+                {loading
+                    ? <Loading minSize={'h-32'} />
                     : (
-                        access.length === 1 
-                        ? ( 
-                            <div className="border">
-                                <h3 className="text-lg text-center font-bold ">Nenhum acesso encontrado</h3>
-                            </div>
-                        ) : (
-                            access.slice(1).map(pessoa => (
-                                <div key={pessoa.idUser} className="rounded-lg py-3 px-5 shadow-[0px_1px_6px_0px_#00000040] dark:shadow-[0px_0px_6px_0px_#ffffff0] dark:border dark:border-dark-600/30">
-                                    <h3 className="text-lg font-bold mb-3">{pessoa.name}</h3>
-                                    <div className="flex justify-between items-center">
-                                        <div className="flex items-center gap-8">
-                                            <DefaultUser user />
-                                            <div>
-                                                <p><b>Email:</b> {pessoa.email}</p>
-                                                <p><b>Tipo de acesso:</b> {pessoa.role}</p>
+                        access.length === 1
+                            ? (
+                                <div className="border rounded-lg py-8 shadow-[0px_1px_6px_0px_#00000040] dark:shadow-[0px_0px_6px_0px_#ffffff0] dark:border dark:border-dark-600/30">
+                                    <h3 className="text-lg text-center font-bold ">Nenhum acesso encontrado</h3>
+                                </div>
+                            ) : (
+                                access.slice(1).map(pessoa => (
+                                    <div key={pessoa.idUser} className="rounded-lg py-3 px-5 shadow-[0px_1px_6px_0px_#00000040] dark:shadow-[0px_0px_6px_0px_#ffffff0] dark:border dark:border-dark-600/30">
+                                        <h3 className="text-lg font-bold mb-3">{pessoa.name}</h3>
+                                        <div className="flex justify-between items-center">
+                                            <div className="flex items-center gap-8">
+                                                <DefaultUser user />
+                                                <div>
+                                                    <p><b>Email:</b> {pessoa.email}</p>
+                                                    <p><b>Tipo de acesso:</b> {pessoa.role}</p>
+                                                </div>
+                                            </div>
+                                            <div className="flex gap-2">
+                                                <button onClick={() => confirmDel(pessoa.idUser)} className="text-vermelho-900 font-semibold p-2 ">Excluir</button>
+                                                <button className="text-white bg-azul-900 p-2 rounded-lg font-semibold">Editar</button>
                                             </div>
                                         </div>
-                                        <div className="flex gap-2">
-                                            <button onClick={() => confirmDel(pessoa.idUser)} className="text-vermelho-900 font-semibold p-2 ">Excluir</button>
-                                            <button className="text-white bg-azul-900 p-2 rounded-lg font-semibold">Editar</button>
-                                        </div>
                                     </div>
-                                </div>
-                            ))
-                        )
+                                ))
+                            )
                     )
                 }
             </div>
@@ -128,30 +144,43 @@ export function Access() {
             <h2 className="text-2xl font-semibold my-8">Novo acesso</h2>
 
             <div className="max-w-xl flex flex-col gap-5">
-                <InputText InputId="name" labelName="Nome completo do funcionário:" required onChange={changeNewAccess} />
+                <InputText
+                    InputId="name"
+                    labelName="Nome completo do funcionário:"
+                    required
+                    onChange={changeNewAccess}
+                    val={newAccess.name}
+                />
 
                 <div className="flex gap-3">
-                    <InputText InputId="email" labelName="E-mail:" required onChange={changeNewAccess} />
+                    <InputText
+                        InputId="email"
+                        labelName="E-mail:"
+                        required
+                        onChange={changeNewAccess}
+                        val={newAccess.email}
+                    />
 
                     <label htmlFor="role">
                         Tipo de acesso: <b className="text-vermelho-900">*</b>
-                        <select
+                        <input
+                            type="text"
                             id="role"
                             name="role"
-                            onChange={handleSelectChange}
-                            className="max-h-[42px] h-full w-full custom-input mt-2"
-                            defaultValue=''
-                        >   
-                            <option value="" disabled>Selecione</option>
-                            <option value="secretaria">Secretaria</option>
-                            <option value="quiropraxista">Quiropraxista</option>
-                        </select>
+                            className="w-full custom-input mt-2 capitalize"
+                            onChange={changeNewAccessList}
+                            list="roles"
+                            value={newAccess.role}
+                        />
+                        <datalist id="roles">
+                            <option value="Secretaria" />
+                            <option value="Quiropraxista" />
+                        </datalist>
+
                     </label>
                 </div>
 
                 <button
-
-
                     className={`bg-azul-900 font-medium flex items-center text-white rounded-lg p-3 w-52 justify-center
                     ${loadingForm ? ' bg-azul-900/50 cursor-not-allowed w-36' : ' bg-azul-900 w-52'}`}
                     onClick={validateForm}
@@ -159,11 +188,11 @@ export function Access() {
                     {loadingForm ? (
                         <span className="w-full flex justify-center z-0">
                             Criando
-                            <LoadingIcon/>
+                            <LoadingIcon />
                         </span>
                     ) : (
                         <span className="w-full flex justify-center items-center">
-                            Criar mais acessos <FaPlus className="ml-2" /> 
+                            Criar mais acessos <FaPlus className="ml-2" />
                         </span>
                     )}
                 </button>
